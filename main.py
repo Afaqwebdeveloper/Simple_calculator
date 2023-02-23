b38201537fe41546d81799adf48b50869376eea7
Afaqwebdeveloper <afaqwebdeveloper@gmail.com>
2023-02-23 22:51:05 +0500

    Added main file

---
+++ main.py
@@ -0,0 +1,14 @@
+from addition import add
+from Multiply import multiply
+from Subtarction import subtract
+
+
+if __name__=="_main_":
+    A=6
+    B=4
+
+
+    print(add(A,B))
+    print(multiply(A,B))
+    print(subtract(A,B))
+          
